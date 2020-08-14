69916780e3c57782faf4b0ee899415c563eeb181
crazould <crazould@gmail.com>
2020-08-14 13:34:40 +0700

    add edit participant

--- src/app/manage-participant/manage-participant.component.ts
+++ src/app/manage-participant/manage-participant.component.ts
@@ -9,6 +9,12 @@ import { Title } from '@angular/platform-browser';
 })
 export class ManageParticipantComponent implements OnInit {
 
+  participants = []
+  participantName : string
+  participantPassword : string
+  participantEmail : string
+
+
   constructor(private titleService : Title) {
     this.setTitle('Manage Participant')
   }
@@ -18,6 +24,45 @@ export class ManageParticipantComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.participants.push({
+      name: "Emilia",
+      email: "Emilia@gmail.com",
+      password: "Emilia123"
+    })
+    this.participants.push({
+      name: "Subaru",
+      email: "Subaru@gmail.com",
+      password: "Subaru123"
+    })
+    this.participants.push({
+      name: "Puck",
+      email: "Puck@gmail.com",
+      password: "Puck123"
+    })
+  }
+
+  setParticipantName(event : any) {
+    this.participantName = event.target.value
+  }
+
+  setParticipantEmail(event : any) {
+    this.participantEmail = event.target.value
+  }
+
+  setParticipantPassword(event : any) {
+    this.participantPassword = event.target.value
+  }
+
+  insertParticipant() {
+    this.participants.push({
+      name: this.participantName,
+      email: this.participantEmail,
+      password: this.participantPassword,
+      isEdit: false,
+      isDelete: false
+    })
+
+    console.log(this.participants)
 
   }
 
