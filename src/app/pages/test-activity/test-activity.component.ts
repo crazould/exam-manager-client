561805c5d1649aad1ec2202dfe6bc142230b8c12
crazould <crazould@gmail.com>
2020-08-21 00:02:23 +0700

    + title test activity

--- src/app/pages/test-activity/test-activity.component.ts
+++ src/app/pages/test-activity/test-activity.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-test-activity',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestActivityComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private titleService: Title
+  ) { 
+    this.setTitle("Test Activity")
+  }
 
   ngOnInit(): void {
+
   }
+
+  setTitle(pageTitle: string): void {
+    this.titleService.setTitle(pageTitle)
+  }
+
 
 }
