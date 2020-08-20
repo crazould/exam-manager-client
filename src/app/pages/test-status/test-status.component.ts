11c5b05654a9da4fef9873c7e5c3df8765396182
crazould <crazould@gmail.com>
2020-08-21 00:06:10 +0700

    + title test status

--- src/app/pages/test-status/test-status.component.ts
+++ src/app/pages/test-status/test-status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-test-status',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestStatusComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private titleService: Title
+  ) {
+    this.setTitle("Test Status")
+  }
 
   ngOnInit(): void {
+  }
+
+  setTitle(pageTitle: string): void {
+    this.titleService.setTitle(pageTitle)
   }
 
 }
