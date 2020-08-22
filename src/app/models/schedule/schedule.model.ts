33d2c498c75fa3d924c24fb48bd223941078ccde
crazould <crazould@gmail.com>
2020-08-22 16:43:20 +0700

    + schedule schema

--- src/app/models/schedule/schedule.model.ts
+++ src/app/models/schedule/schedule.model.ts
@@ -1,2 +1,22 @@
+import { Test } from '../test/test.model'
+import { Participant } from '../participant/participant.model'
+
 export class Schedule {
+
+    id: number
+    test: Test
+    startTime: string
+    endTime: string
+
+    participants: Participant[]
+    status: string
+
+    constructor(test: Test, startTime: string, endTime: string) {
+        this.test = test
+        this.startTime = startTime
+        this.endTime = endTime
+    }
+
+    
+
 }
